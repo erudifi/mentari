ec6fcad76d3debea62908b7beb7cedd426144a85
Faisal Amir <faisal@danacita.com>
2019-03-08 09:02:08 +0000

    Remove lorem

--- src/lib/Floating/Whatsapp.jsx
+++ src/lib/Floating/Whatsapp.jsx
@@ -20,11 +20,6 @@ const FloatingWhatsAppButton = ({ phoneNumber }) => (
           zIndex: 100
         }}
       />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque laboriosam dicta non at
-        deleniti sapiente magni dolore illum, debitis molestias enim adipisci tenetur eum
-        repudiandae facilis harum vero perspiciatis iste?
-      </p>
     </a>
   </React.Fragment>
 );
